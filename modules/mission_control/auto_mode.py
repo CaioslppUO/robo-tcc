8116a8b20e64a97dafa3c76aabf05e2893aee7f5
Lucas <lucasgrafimar@gmail.com>
2022-07-06 13:46:22 -0300

    start of mission system development

--- modules/mission_control/auto_mode.py
+++ modules/mission_control/auto_mode.py
@@ -4,11 +4,13 @@
 Control the automatic mode.
 """
 
+from sqlalchemy import Float, true
 import rospy
 from std_msgs.msg import String
 from agrobot_services.log import Log
 from agrobot_services.runtime_log import RuntimeLog
-from mission import Missions
+from mission import Missions, _Location
+from agrobot.msg import Coords
 
 # Auto Mode node
 rospy.init_node('auto_mode', anonymous=True)
@@ -19,37 +21,80 @@ runtime_log: RuntimeLog = RuntimeLog("encoder.py")
 
 # Global control variables
 stop_mission = False
-lidar_can_move = True
-robot_latitude = None
-robot_longitude = None
+lidar_can_move: bool = True
+robot_latitude: float = 0.0
+robot_longitude: float = 0.0
 robot_compass = None
+
 
 def run(missions: Missions) -> None:
     """
     Execute the mission.
     """
-    while(not stop_mission):
-        for mission in missions.get_missions(): # Execute every mission
-            if(stop_mission):
+    while (not stop_mission):
+        for mission in missions.get_missions():  # Execute every mission
+            if (stop_mission):
                 return
 
-            for location in mission.get_locations(): # Execute every location
-                if(stop_mission):
-                    return 
+            for location in mission.get_locations():  # Execute every location
+                if (stop_mission):
+                    return
 
-                while(location.latitude != robot_latitude and location.longitude != robot_longitude): # Verifica se chegou ao destino da localização.
-                    if(stop_mission):
+                while verify_coordinates(
+                        location
+                ):  # Verifica se chegou ao destino da localização.
+                    if (stop_mission):
                         return None
 
-                    if(lidar_can_move):
-                        need_to_correct_route = True # Verificar se precisa ou não corrigir a direção
-                        while(need_to_correct_route): # Lógica para corrigir a direção do robô
-                            if(stop_mission):
+                    if (lidar_can_move):
+                        while need_to_correct_route(
+                                location
+                        ):  # Lógica para corrigir a direção do robô
+                            if (stop_mission):
                                 return None
-                                 
+
                         # Vai reto
                     else:
-                        runtime_log.info("Can't move, object in front of robot (lidar).")
+                        runtime_log.info(
+                            "Can't move, object in front of robot (lidar).")
+
+
+def need_to_correct_route(location: _Location) -> bool:
+    global robot_compass
+    """
+    Verify if the robot needs to correct the route.
+    """
+
+    new_robot_latitude: float = 0.0
+    new_robot_longitude: float = 0.0
+    new_latitude: float = location.latitude - robot_latitude
+    new_longitude: float = location.longitude - robot_longitude
+
+    if robot_latitude == 0 and robot_longitude == 0:
+        runtime_log.warning("Robot cold not get GPS coordinates.")
+        return False
+    if new_longitude < 0:  # Quadrante baixo
+        if new_longitude < 0:  # Quadrante esquerda
+
+            return True
+        else:  # Quadrante direita
+            return False
+    else:  # Quadrante cima
+        if new_longitude < 0:  # Quadrante esquerda
+            return True
+        else:  # Quadrante direita
+            return False
+
+
+def verify_coordinates(location: _Location) -> bool:
+    """
+    Verify if the robot is in the correct position.
+    """
+    if (location.latitude != robot_latitude
+            or location.longitude != robot_longitude):
+        return True
+    return False
+
 
 def callback_lidar(dt: String):
     """
@@ -57,6 +102,7 @@ def callback_lidar(dt: String):
     """
     global lidar_can_move
     lidar_can_move = bool(dt.data)
+
 
 def callback_start_mission(dt: String):
     """
@@ -66,6 +112,7 @@ def callback_start_mission(dt: String):
     missions.load_mission_file()
     run(missions)
 
+
 def callback_stop_mission(dt: String):
     """
     Stop an mission that is being executed.
@@ -73,13 +120,15 @@ def callback_stop_mission(dt: String):
     global stop_mission
     stop_mission = False
 
-def callback_gps(dt: String) -> None:
+
+def callback_gps(dt: Coords) -> None:
     """
     Update current robot latitude and longitude
     """
     global robot_latitude, robot_longitude
-    robot_latitude = float(dt.data.split(";")[0])
-    robot_longitude = float(dt.data.split(";")[1])
+    robot_latitude = float(dt.latitude)
+    robot_longitude = float(dt.longitude)
+
 
 def callback_compass(dt: String) -> None:
     """
@@ -88,13 +137,39 @@ def callback_compass(dt: String) -> None:
     global robot_compass
     robot_compass = float(dt.data)
 
+
 if __name__ == "__main__":
     try:
         rospy.Subscriber('/lidar', String, callback_lidar)
-        rospy.Subscriber('/gps', String, callback_gps)
+        rospy.Subscriber('/gps', Coords, callback_gps)
         rospy.Subscriber('/compass', String, callback_compass)
         rospy.Subscriber('/start_mission', String, callback_start_mission)
         rospy.Subscriber('/stop_mission', String, callback_stop_mission)
         rospy.spin()
     except:
         runtime_log.error("Auto Mode died. Check logs file.")
+
+    #     if(a <= b-3 or a >= b+3):
+    #   if(b >= 0 and b <= 90):
+    #     if(a <= 180 and a > b):
+    #       dir = esquerda()
+    #       print("1")
+    #     else:
+    #       dir = direita()
+    #       print("2")
+    #   elif(b > 90 and b <= 270):
+    #     if(a > b):
+    #       dir = esquerda()
+    #       print("3")
+    #     else:
+    #       print("4")
+    #       dir = direita()
+    #   else:
+    #     if(a >= 180 and a < b):
+    #       dir = direita()
+    #       print("5")
+    #     else:
+    #       print("6")
+    #       dir = direita()
+    # else:
+    #   dir = reto()
