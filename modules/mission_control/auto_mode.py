5e0535e2438904610a4bcb5d6ad47fdb50113290
CaioslppUO <caioslppuo@gmail.com>
2022-07-11 17:16:02 -0300

    Auto Mode Refactoring

--- modules/mission_control/auto_mode.py
+++ modules/mission_control/auto_mode.py
@@ -4,20 +4,18 @@
 Control the automatic mode.
 """
 
-import rospy
+import rospy, time
 from std_msgs.msg import String
 from agrobot_services.log import Log
 from agrobot_services.runtime_log import RuntimeLog
 from mission import Missions, _Location
-from agrobot.msg import Coords,Control
+from agrobot.msg import Coords, Control
 import auto_mode_calcs
-
-
 
 # Auto Mode node
 rospy.init_node('auto_mode', anonymous=True)
 
-pub = rospy.Publisher("/auto_mode", Control, queue_size=10)
+pub = rospy.Publisher("/get_robot_commands", Control, queue_size=10)
 
 # Log class
 log: Log = Log("auto_mode.py")
@@ -30,124 +28,121 @@ robot_latitude: float = 0.0
 robot_longitude: float = 0.0
 robot_compass: float = 0.0
 
+def verify_coordinates(location: _Location) -> bool:
+    """
+    Verify if the robot is in the correct position.
+    """
+    decimals = 4
+    if (round(location.latitude, decimals) != round(robot_latitude, decimals) or round(location.longitude, decimals) != round(robot_longitude, decimals)):
+        return True
+    return False
+
+def send_control_command(speed: float, steer: float, timeout: float) -> None:
+    control: Control = Control()
+    control.speed = speed
+    control.steer = steer
+    pub.publish(control)
+    time.sleep(timeout)
+
+def turn_left() -> None:
+    turn_left_timeout = 0.5
+    runtime_log.info("Turn Left")
+    send_control_command(0.0, -0.5, turn_left_timeout)
+    send_control_command(0.5, 0.0, turn_left_timeout)
+    send_control_command(0.0, 0.5, turn_left_timeout)
+    send_control_command(0.5, 0.0, turn_left_timeout)
+    send_control_command(0.0, 0.0, 0.0)
+
+def turn_right() -> None:
+    turn_right_timeout = 0.5
+    runtime_log.info("Turn Right")
+    send_control_command(0.0, 0.5, turn_right_timeout)
+    send_control_command(0.5, 0.0, turn_right_timeout)
+    send_control_command(0.0, -0.5, turn_right_timeout)
+    send_control_command(0.5, 0.0, turn_right_timeout)
+    send_control_command(0.0, 0.0, 0.0)
+
+def go_forward() -> None:
+    go_forward_timeout = 0.0
+    runtime_log.info("Go Forward")
+    send_control_command(0.5, 0.0, go_forward_timeout)
 
 def run(missions: Missions) -> None:
     """
     Execute the mission.
     """
-    direct_of_correct:int = 0
+    direct_of_correct: str = need_to_correct_route(location)
     control: Control = Control()
     control.speed = 1
     while (not stop_mission):
         for mission in missions.get_missions():  # Execute every mission
             if (stop_mission): return
+
             for location in mission.get_locations():  # Execute every location
                 if (stop_mission): return
+
                 while verify_coordinates(location):  # Verifica se chegou ao destino da localização.
-                    if (stop_mission): return None
+                    if (stop_mission): return
+
                     if (lidar_can_move):
-                        while direct_of_correct != 2:  # Lógica para corrigir a direção do robô
-                            direct_of_correct = need_to_correct_route(location)
-                            if direct_of_correct == 0:
-                                runtime_log.info("Virar esquerda")
-                                control.speed = 0.0
-                                control.steer = -0.5
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.5
-                                control.steer = 0
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.0
-                                control.steer = 0.5
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.5
-                                control.steer = 0
-                                pub.publish(control)
-                                rospy.sleep(1)
-                            elif direct_of_correct == 1:
-                                runtime_log.info("Virar direita")
-                                control.speed = 0.0
-                                control.steer = 0.5
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.5
-                                control.steer = 0
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.0
-                                control.steer = -0.5
-                                pub.publish(control)
-                                rospy.sleep(1)
-                                control.speed = 0.5
-                                control.steer = 0
-                                pub.publish(control)
-                                rospy.sleep(1)
+                        while direct_of_correct != "left" and direct_of_correct != "right":  # Lógica para corrigir a direção do robô
                             if (stop_mission): return None
-                        control.speed = 0.8
-                        control.steer = 0
-                        pub.publish(control)
+                            
+                            direct_of_correct = need_to_correct_route(location)
+
+                            if direct_of_correct == "left":
+                                turn_left()
+                            elif direct_of_correct == "right":
+                                turn_right()
+
+                        go_forward()
                     else:
                         runtime_log.info("Can't move, object in front of robot (lidar).")
 
 
-def need_to_correct_route(location: _Location) -> int:
-    global robot_compass
+def need_to_correct_route(location: _Location) -> str:
     """
     Verify if the robot needs to correct the route.
     """
+    global robot_compass
 
+    # Setting robot in (0,0) coordinate in the system
     new_robot_latitude: float = 0.0
     new_robot_longitude: float = 0.0
+
+    # Setting the mission lat and lon to be relative to robot (0,0)
     new_latitude: float = location.latitude - robot_latitude
     new_longitude: float = location.longitude - robot_longitude
 
-    if robot_latitude == 0 and robot_longitude == 0:
-        runtime_log.warning("Robot cold not get GPS coordinates.")
-        return 2
+    # Error trying to read robot lat and lon
+    if robot_latitude >= 0 or robot_longitude >= 0:
+        runtime_log.warning("Robot cold not read GPS coordinates.")
+        return "error"
+
+    H = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_latitude, new_longitude)
+    co = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_robot_latitude, new_longitude)
+    Beta = 90 - auto_mode_calcs.get_angle_asin(co, H)
+
     if new_longitude < 0:  # Quadrante baixo
-        if new_longitude < 0:  # Quadrante esquerda
-            H = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_latitude, new_longitude)
-            co = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_robot_latitude, new_longitude)
-            Beta = 90- auto_mode_calcs.get_angle_asin(co,H)
-            Beta = Beta+180
+        if new_latitude < 0:  # Quadrante esquerda
+            Beta = Beta + 180
             if robot_compass >= Beta or robot_compass <= Beta-180:
-                return 0 #Virar esquerda
-            return 1 #Virar direita
+                return "left" #Virar esquerda
+            return "right" #Virar direita
         else:  # Quadrante direita
-            H = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_latitude, new_longitude)
-            co = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_robot_latitude, new_longitude)
-            Beta = 90- auto_mode_calcs.get_angle_asin(co,H)
             Beta = 180 - Beta
             if robot_compass <= Beta or robot_compass >=  Beta+180:
-                return 1 #Virar Direita
-            return 0 #Virar esquerda
+                return "right" #Virar Direita
+            return "left" #Virar esquerda
     else:  # Quadrante cima
-        if new_longitude < 0:  # Quadrante esquerda
-            H = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_latitude, new_longitude)
-            co = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_robot_latitude, new_longitude)
-            Beta = 90- auto_mode_calcs.get_angle_asin(co,H)
+        if new_latitude < 0:  # Quadrante esquerda
             if 360-Beta >= robot_compass or robot_compass <= 180-Beta:
-                return 0 #Virar Esquerda
-            return 1 #Virar Direita
+                return "left" #Virar Esquerda
+            return "right" #Virar Direita
         else:  # Quadrante direita
-            H = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_latitude, new_longitude)
-            co = auto_mode_calcs.dist_two_points(new_robot_latitude, new_robot_longitude, new_robot_latitude, new_longitude)
-            Beta = 90- auto_mode_calcs.get_angle_asin(co,H)
             if robot_compass >= 360-Beta or robot_compass <= Beta:
-                return 1 #Virar Direita
-            return 0 #Virar Esquerda
-    return 2
-
-def verify_coordinates(location: _Location) -> bool:
-    """
-    Verify if the robot is in the correct position.
-    """
-    if (round(location.latitude,4) != round(robot_latitude,4) or round(location.longitude,4) != round(robot_longitude,4)):
-        return True
-    return False
-
+                return "right" #Virar Direita
+            return "left" #Virar Esquerda
 
 def callback_lidar(dt: String):
     """
