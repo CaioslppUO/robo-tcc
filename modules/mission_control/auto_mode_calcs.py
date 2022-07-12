e75bc888806d4d68983c8052959445c60aee8886
CaioslppUO <caioslppuo@gmail.com>
2022-07-11 23:19:35 -0300

    Added forward option in auto_mode correct direction

--- modules/mission_control/auto_mode_calcs.py
+++ modules/mission_control/auto_mode_calcs.py
@@ -42,23 +42,48 @@ def need_to_correct_route(location: _Location, robot_lat: float, robot_lon: floa
     co = dist_two_points(robot_lat, robot_lon, robot_lat, mission_lon)
     Beta = 90 - get_angle_asin(co, H)
 
+    margin = 10
+
     if mission_lon < 0:  # Quadrante baixo
         if mission_lat < 0:  # Quadrante esquerda
             Beta = Beta + 180
+            print("Compass: {:.4f}, Beta: {:.4f}".format(round(compass, 4), round(Beta, 4)))
+
+            if(compass <= Beta + margin and compass >= Beta + margin):
+                return "forward"
+
             if compass >= Beta or compass <= Beta-180:
                 return "left" #Virar esquerda
             return "right" #Virar direita
         else:  # Quadrante direita
             Beta = 180 - Beta
+            print("Compass: {:.4f}, Beta: {:.4f}".format(round(compass, 4), round(Beta, 4)))
+
+            if(compass <= Beta + margin and compass >= Beta + margin):
+                return "forward"
+
             if compass <= Beta or compass >=  Beta+180:
                 return "right" #Virar Direita
             return "left" #Virar esquerda
     else:  # Quadrante cima
         if mission_lat < 0:  # Quadrante esquerda
+            print("Compass: {:.4f}, Beta: {:.4f}".format(round(compass, 4), round(Beta, 4)))
+            dif = 360 - Beta
+            if(compass >= dif - margin and compass <= dif + margin ):
+                return "forward"
+            elif((dif + margin > 360 and compass <= dif + margin - 360)):
+                return "forward"
+
             if (compass >= 0 and compass <= 180-Beta) or (compass >= 360-Beta):
                 return "left"
             return "right"
         else:  # Quadrante direita
+            print("Compass: {:.4f}, Beta: {:.4f}".format(round(compass, 4), round(Beta, 4)))
+            if((compass >= Beta and compass <= Beta + margin) or (compass >= 0 and compass >= Beta - margin and compass <= Beta + margin)):
+                return "forward"
+            elif(Beta - margin < 0 and compass >= 360 + (Beta - margin)):
+                return "forward"
+
             if (compass <= 180+Beta and compass >= 90) or (compass > Beta and compass <= 90):
                 return "left"
             return "right"
