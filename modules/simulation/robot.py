49795e552166ade890dfc173e2fed130ef8338d6
CaioslppUO <caioslppuo@gmail.com>
2022-07-19 22:22:09 -0300

    Bug corrections in robot simulation

--- modules/simulation/robot.py
+++ modules/simulation/robot.py
@@ -55,14 +55,42 @@ class Robot:
 
     def forward(self, last_lon: float) -> "tuple[float, float]":
         """
-        Move the robot forward using the speed_factor.
+        Move the robot forward.
         """
         if(self.quadrant == 1 or self.quadrant == 2):
-            new_x = last_lon + self.delta_x
+            new_x = round(last_lon + self.delta_x, 5)
         else:
-            new_x = last_lon - self.delta_x
+            new_x = round(last_lon - self.delta_x, 5)
         new_y = self.get_y(new_x)
-        return new_x, new_y
+        return new_y, new_x
+
+    def turn_left(self, last_lon: float) -> "tuple[float, float]":
+        """
+        Turn the robot to the left by 5 degrees.
+        """
+        # Update the line angular coefficient
+        if(self.quadrant == 1 or self.quadrant == 3):
+            self.update_slope(self.slope_degrees - 5)
+        elif(self.quadrant == 2 or self.quadrant == 4):
+            self.update_slope(self.slope_degrees + 5)            
+        else: # infinite
+            self.update_slope(self.slope_degrees + 5)
+
+        return self.forward(last_lon)
+
+    def turn_right(self, last_lon: float) -> "tuple[float, float]":
+        """
+        Turn the robot to the right by 5 degrees.
+        """
+        # Update the line angular coefficient
+        if(self.quadrant == 1 or self.quadrant == 3):
+            self.update_slope(self.slope_degrees + 5)
+        elif(self.quadrant == 2 or self.quadrant == 4):
+            self.update_slope(self.slope_degrees - 5)            
+        else: # infinite
+            self.update_slope(self.slope_degrees + 5)
+
+        return self.forward(last_lon)
 
     def update_slope(self, new_slope_in_degrees: float) -> None:
         """
@@ -78,26 +106,24 @@ class Robot:
             self.slope_degrees =  self.slope_degrees_with_signal + 180
         else:
             self.slope_degrees = self.slope_degrees_with_signal
-            
-        print("Slope:", self.slope)
-        print("Slope degrees:", self.slope_degrees)
-        print("Slope degrees with signal:", self.slope_degrees_with_signal)
-        print("Slope degrees converted to radians", round(math.radians(self.slope_degrees), 5))
-        print("Slope degrees converted to radians tan", round(math.tan(math.radians(self.slope_degrees)), 5))
 
-#r = Robot(-25.43548, -54.59671, -25.43545, -54.59669)
+r = Robot(-25.43548, -54.59671, -25.43545, -54.59669)
 
-#r.forward(-54.59669)
+print(-25.43548, -54.59671)
+print(-25.43545, -54.59669)
 
-r = Robot(0, 0, -1, 1)
-#r.update_slope(1.0)
-print("Point: ", r.forward(0))
-print(r.slope)
-print(r.slope_degrees)
-print(r.slope_degrees_with_signal)
-print(r.quadrant)
-r.update_slope(r.slope_degrees + 5)
-
+new_lat, new_lon = r.forward(-54.59669)
+print(new_lat, new_lon, r.slope, r.slope_degrees)
+new_lat, new_lon = r.turn_left(new_lon)
+print(new_lat, new_lon, r.slope, r.slope_degrees)
+new_lat, new_lon = r.turn_right(new_lon)
+print(new_lat, new_lon, r.slope, r.slope_degrees)
+new_lat, new_lon = r.forward(new_lon)
+print(new_lat, new_lon, r.slope, r.slope_degrees)
+#new_lat, new_lon = r.turn_right(new_lon)
+#print(new_lat, new_lon, r.slope, r.slope_degrees)
+#new_lat, new_lon = r.turn_right(new_lon)
+#print(new_lat, new_lon, r.slope, r.slope_degrees)
 
 
 
