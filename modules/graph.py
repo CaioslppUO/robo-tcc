1244aefdb80ae2d0c51e6884ad91a338bda8aa73
Lucas <lucasgrafimar@gmail.com>
2022-07-23 11:38:36 -0300

    separate the class that takes care of the graph data from the class that displays the graph

--- modules/graph.py
+++ modules/graph.py
@@ -1,13 +1,12 @@
-from matplotlib.markers import MarkerStyle
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import json
 
 plt.style.use('seaborn-whitegrid')
 from geometry import Point
 
-class Graph:
-    def __init__(self,interval:int = 2000):
-        self.interval:int = interval
+class GraphData:
+    def __init__(self):
         self.straight_from_mission_lon:"list[float]"  = []
         self.straight_from_mission_lat:"list[float]"  = []
         self.robot_position:"tuple[float,float]" = None
@@ -15,71 +14,31 @@ class Graph:
         self.robot_route_lat:"list[float]"  = []
         self.robot_route_lon:"list[float]"  = []
         self.correction_direction:"str" = None
-        try:
-            plt.get_current_fig_manager().window.attributes('-zoomed', True)
-        except:
-            pass
-    def run(self):
+        self.fieldnames = ["straight_from_mission_lon", "straight_from_mission_lat", "robot_position", "closest_point", "robot_route_lat", "robot_route_lon", "correction_direction"]
+
+    def __write_to_csv(self):
         """
-        Run the graph
+        Write the data to a csv file
         """
-        self.animation = FuncAnimation(plt.gcf(), self.update, interval=self.interval)
-        plt.show()
-
-    def update(self,frame):
-        """
-        Update the graph
-        """
-        plt.xlim(min(self.straight_from_mission_lon), max(self.straight_from_mission_lon))
-        plt.ylim(min(self.straight_from_mission_lat), max(self.straight_from_mission_lat))
-        plt.xlabel("LON")
-        plt.ylabel("LAT")
-
-        plt.cla()
-
-        # Plot mission line
-        plt.plot(self.straight_from_mission_lon, self.straight_from_mission_lat, marker='o', markerfacecolor='blue', markersize=6, color='blue', label='Missão')
-
-        #Plot robot route
-        if(len(self.robot_route_lon) > 0):
-            plt.plot(self.robot_route_lon, self.robot_route_lat, marker='o', markerfacecolor='green', markersize=6, color='green', label='Rota do robô')
-
-        #Plot robot position
-        if(self.robot_position is not None):
-            plt.plot(self.robot_position[0], self.robot_position[1], marker='o', markerfacecolor='red', markersize=6, color='red', label='Robô')
-
-        #Plot closest point
-        if(self.closest_point is not None):
-            plt.plot(self.closest_point[0], self.closest_point[1], marker='o', markerfacecolor='black', markersize=6, color='black', label='Ponto mais próximo')
-
-        if(self.correction_direction is not None): # Add correction_direction to the legend
-            plt.plot([0], [0], markersize=0.1 , marker="o", markeredgecolor="green", markerfacecolor="blue", label="Direção de correção: {}".format(self.correction_direction))
-            # Plotting correction direction
-            plt.annotate("",
-                xy=(0, self.straight_from_mission_lat[-1]),
-                xytext=(0, self.straight_from_mission_lat[-1]),
-                va="center",
-                ha="right",
-                arrowprops={"arrowstyle": "-", "lw": 0},
-                label="Direção de correção: {}".format(self.correction_direction))
-
-        #Plotting mission direction
-        plt.annotate("",
-            xy=(self.straight_from_mission_lon[-1], self.straight_from_mission_lat[-1]),
-            xytext=(self.straight_from_mission_lon[0], self.straight_from_mission_lat[0]),
-            va="center",
-            ha="right",
-            arrowprops={"arrowstyle": "-|>", "lw": 0.5})
-
-        plt.legend(loc="upper left")
-        plt.tight_layout()
-        
+        with open('graph.json', 'w') as outfile:
+            info = {
+                "straight_from_mission_lon": self.straight_from_mission_lon,
+                "straight_from_mission_lat": self.straight_from_mission_lat,
+                "robot_position": self.robot_position,
+                "closest_point": self.closest_point,
+                "robot_route_lat": self.robot_route_lat,
+                "robot_route_lon": self.robot_route_lon,
+                "correction_direction": self.correction_direction
+            }
+            outfile.write(json.dumps(info))
+            print(json.dumps(info))
 
     def set_corretion_direction_legend(self,direction:str):
         """
         Set the correction direction legend
         """
         self.correction_direction = direction
+        self.__write_to_csv()
 
     def set_straight_from_mission(self,points:"list[Point]"):
         """
@@ -91,6 +50,7 @@ class Graph:
         for point in points:
             self.straight_from_mission_lat.append(point.get_point()[0])
             self.straight_from_mission_lon.append(point.get_point()[1])
+        self.__write_to_csv()
 
     def new_position_robot(self,position:"tuple[float,float]"):
         """
@@ -99,16 +59,97 @@ class Graph:
         self.robot_route_lon.append(position[0])
         self.robot_route_lat.append(position[1])
         self.robot_position = position;
+        self.__write_to_csv()
 
-def test_func():
-    test = Graph()
+class Graph:
+    def __init__(self,interval:int = 2000):
+        self.interval:int = interval
+        try:
+            plt.get_current_fig_manager().window.attributes('-zoomed', True)
+        except:
+            pass
+
+    def run(self):
+        """
+        Run the graph
+        """
+        self.animation = FuncAnimation(plt.gcf(), self.update, interval=self.interval)
+        plt.show()
+
+    def update(self,frame):
+        """
+        Update the graph
+        """
+        try:
+            with open('graph.json') as json_file:
+                data = json.load(json_file)
+        except:
+            data = None
+        plt.cla()
+        plt.xlabel("LON")
+        plt.ylabel("LAT")
+        if data is not None:
+            if(len(data["straight_from_mission_lon"]) > 0):
+                plt.xlim(min(data["straight_from_mission_lon"]), max(data["straight_from_mission_lon"]))
+                plt.ylim(min(data["straight_from_mission_lat"]), max(data["straight_from_mission_lat"]))
+                
+                # Plot mission line
+                plt.plot(data["straight_from_mission_lon"], data["straight_from_mission_lat"], marker='o', markerfacecolor='blue', markersize=6, color='blue', label='Missão')
+                #Plotting mission direction
+                plt.annotate("",
+                    xy=(data["straight_from_mission_lon"][-1], data["straight_from_mission_lat"][-1]),
+                    xytext=(data["straight_from_mission_lon"][0], data["straight_from_mission_lat"][0]),
+                    va="center",
+                    ha="right",
+                    arrowprops={"arrowstyle": "-|>", "lw": 0.5})
+
+            #Plot robot route
+            if(len(data["robot_route_lon"]) > 0):
+                plt.plot(data["robot_route_lon"], data["robot_route_lat"], marker='o', markerfacecolor='green', markersize=6, color='green', label='Rota do robô')
+
+            #Plot robot position
+            if(data["robot_position"] is not None):
+                plt.plot(data["robot_position"][0], data["robot_position"][1], marker='o', markerfacecolor='red', markersize=6, color='red', label='Robô')
+
+            #Plot closest point
+            if(data["closest_point"] is not None):
+                plt.plot(data["closest_point"][0], data["closest_point"][1], marker='o', markerfacecolor='black', markersize=6, color='black', label='Ponto mais próximo')
+
+            if(data["correction_direction"] is not None): # Add correction_direction to the legend
+                plt.plot([0], [0], markersize=0.1 , marker="o", markeredgecolor="green", markerfacecolor="blue", label="Direção de correção: {}".format(self.correction_direction))
+                # Plotting correction direction
+                plt.annotate("",
+                    xy=(0, data["straight_from_mission_lat"][-1]),
+                    xytext=(0, data["straight_from_mission_lat"][-1]),
+                    va="center",
+                    ha="right",
+                    arrowprops={"arrowstyle": "-", "lw": 0},
+                    label="Direção de correção: {}".format(data["correction_direction"]))
+
+        plt.legend(loc="upper left")
+        plt.tight_layout()
+
+
+def write_graph():
+    """
+    Exemplo de como escrever um gráfico
+    """
+    gpData = GraphData()
     lineTest = []
     lineTest.append(Point(0,0))
     lineTest.append(Point(1,1))
     lineTest.append(Point(2,2))
     lineTest.append(Point(3,3))
-    test.set_straight_from_mission(lineTest)
-    test.new_position_robot((3,1))
-    test.run()
+    gpData.set_straight_from_mission(lineTest)
+    gpData.new_position_robot((3,1))
+    gpData.new_position_robot((3,2))
+    gpData.new_position_robot((2,2))
 
-# test_func()
+def show_graph():
+    """
+    Chamar essa função em um terminal separado
+    """
+    Graph().run()
+
+# Graph().run()
+write_graph()
